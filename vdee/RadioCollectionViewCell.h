b4607080534909141dd31dc924f07e760d8bc2e0
jesumperez18 <jesuperez@csumb.edu>
2018-04-23 18:50:37 -0700

    Added proper Activity indicator flow

--- vdee/RadioCollectionViewCell.h
+++ vdee/RadioCollectionViewCell.h
@@ -2,24 +2,17 @@
 //  radioCollectionViewCell.h
 //  vdee
 //
-//  Created by Jesus perez on 4/4/18.
+//  Created by Jesus perez on 4/18/18.
 //  Copyright © 2018 Anita Garcia. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-
 @interface radioCollectionViewCell : UICollectionViewCell
-
-//@property (weak, nonatomic) IBOutlet UIButton *playButton;
 @property (weak, nonatomic) IBOutlet UIButton *playButton;
-@property NSInteger position;
 @property (weak, nonatomic) IBOutlet UILabel *label;
-
-
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *act;
 
 
 
 @end
-
-
--- vdee/radioCollectionViewCell.m
+++ vdee/radioCollectionViewCell.m
@@ -2,19 +2,12 @@
 //  radioCollectionViewCell.m
 //  vdee
 //
-//  Created by Jesus perez on 4/4/18.
+//  Created by Jesus perez on 4/18/18.
 //  Copyright © 2018 Anita Garcia. All rights reserved.
 //
 
 #import "radioCollectionViewCell.h"
 
-
-
 @implementation radioCollectionViewCell
 
-
-
-
 @end
-
-
